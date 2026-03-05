6fd5f833db69518828309af6260452c3a24841f0
Slava Khlichkin <coolichiki@gmail.com>
2026-03-05 21:47:51 +0100

    Initial Commit

---
+++ Places/PlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlacesApp.swift
+//  Places
+//
+//  Created by Slava Khlichkin on 05/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct PlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlacesTests/PlacesTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlacesTests.swift
+//  PlacesTests
+//
+//  Created by Slava Khlichkin on 05/03/2026.
+//
+
+import Testing
+@testable import Places
+
+struct PlacesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
